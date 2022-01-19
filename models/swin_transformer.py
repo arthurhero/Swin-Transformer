8b848a2bac3fb407d0c074f79e51af62566593fa
Ziwen <chenziw@oregonstate.edu>
2022-01-18 17:19:37 -0800

    del ext rep

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -271,7 +271,7 @@ class SwinTransformerBlock(nn.Module):
         return x
 
     def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
+        return f"dim={self.dim}, num_heads={self.num_heads}, " \
                f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
 
     def flops(self):
@@ -331,7 +331,7 @@ class PatchMerging(nn.Module):
         return x
 
     def extra_repr(self) -> str:
-        return f"input_resolution={self.input_resolution}, dim={self.dim}"
+        return f"dim={self.dim}"
 
     def flops(self):
         flops = 0
@@ -402,7 +402,7 @@ class BasicLayer(nn.Module):
         return x
 
     def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, depth={self.depth}"
+        return f"dim={self.dim}, depth={self.depth}"
 
     def flops(self):
         flops = 0
--- run_tiny.sh
+++ run_tiny.sh
@@ -1,4 +1,4 @@
-NUM_PROC=1
+NUM_PROC=4
 #CONFIG_FILE=configs/swin_tiny_patch4_window7_224.yaml
 CONFIG_FILE=configs/swin_tiny_patch2_window4_cifar.yaml
 #RESUME_FILE=checkpoints/swin_tiny_patch4_window7_224.pth
@@ -6,6 +6,7 @@ RESUME_FILE=checkpoints/swin_tiny_patch2_window4_cifar.pth
 #DATA_PATH=../datasets/imagenet/
 DATA_PATH=../datasets/cifar10/
 
-CUDA_VISIBLE_DEVICES=0 WORLD_SIZE=$NUM_PROC python -m torch.distributed.launch --nproc_per_node $NUM_PROC --master_port 12345 \
-    main.py --cfg $CONFIG_FILE --resume $RESUME_FILE \
-    --data-path $DATA_PATH 
+CUDA_VISIBLE_DEVICES=0,1,2,3 WORLD_SIZE=$NUM_PROC python -m torch.distributed.launch --nproc_per_node $NUM_PROC --master_port 12345 \
+    main.py --cfg $CONFIG_FILE \
+    --data-path $DATA_PATH \
+    --resume $RESUME_FILE
