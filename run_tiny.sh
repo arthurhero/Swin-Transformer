fae39b8d7b80551b8d095d664a17f1182d0a9601
arthurhero <chenziwe@grinnell.edu>
2022-01-29 15:48:55 -0800

    save to only one pth file

--- run_tiny.sh
+++ run_tiny.sh
@@ -1,7 +1,6 @@
 NUM_PROC=4
 #CONFIG_FILE=configs/swin_tiny_patch4_window7_224.yaml
 CONFIG_FILE=configs/swin_tiny_patch1_window2_cifar_multi.yaml
-#RESUME_FILE=checkpoints/swin_tiny_patch4_window7_224.pth
 #DATA_PATH=../datasets/imagenet/
 DATA_PATH=../datasets/cifar10/
 
@@ -9,4 +8,3 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 WORLD_SIZE=$NUM_PROC python -m torch.distributed.la
     main.py --cfg $CONFIG_FILE \
     --data-path $DATA_PATH \
     --eval
-#    --resume $RESUME_FILE
