13e2326f6e75ebcffc34b536d523dfb9e6b6e13d
Ziwen <chenziw@oregonstate.edu>
2022-01-29 22:47:04 -0800

    create env sh

---
+++ create_env.sh
@@ -0,0 +1,11 @@
+conda create -n myenv python=3.9
+conda activate myenv
+conda install pytorch torchvision cudatoolkit=11.3 -c pytorch
+conda install -c anaconda cmake
+pip install pykeops
+conda install -c conda-forge pkg-config 
+conda install -c conda-forge libjpeg-turbo 
+conda install -c conda-forge opencv 
+pip install numba
+pip install cupy-cuda113
+pip install ffcv
