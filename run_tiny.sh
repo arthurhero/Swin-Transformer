ac14dee7c63d2039fecb484a9a6e12e35c83c978
arthurhero <chenziwe@grinnell.edu>
2022-05-01 16:19:14 -0700

    ready for imagenet

--- run_tiny.sh
+++ run_tiny.sh
@@ -2,15 +2,14 @@ NUM_PROC=4
 #CONFIG_FILE=configs/swin_tiny_patch4_window7_224.yaml
 #CONFIG_FILE=configs/swin_tiny_patch1_window2_cifar.yaml
 #CONFIG_FILE=configs/resnet50_cifar_multi.yaml
-CONFIG_FILE=configs/cluster_patch1_cifar.yaml
+CONFIG_FILE=configs/cluster_imagenet.yaml
 
-#DATA_PATH=../datasets/imagenet/
-DATA_PATH=../datasets/cifar10/
+DATA_PATH=../datasets/imagenet/
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 WORLD_SIZE=$NUM_PROC python -m torch.distributed.launch --nproc_per_node $NUM_PROC --master_port 12344 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 WORLD_SIZE=$NUM_PROC python -m torch.distributed.launch --nproc_per_node $NUM_PROC --master_port 12344 \
     main.py --cfg $CONFIG_FILE \
     --data-path $DATA_PATH \
-    --batch-size 1024 \
+    --batch-size 16 \
 #    --amp-opt-level O0 \
 #    --eval
 #    --throughput
